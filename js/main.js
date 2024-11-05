7348413c3b1dee9706892c76fc54aa47c0b388ed
Kogaan <vilenshmidt@gmail.com>
2024-11-05 22:14:16 +0500

    Script works

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
+var shot = true
+
 
 function ButtonPress() {
+    if (shot == false){return}
     setTimeout(function(){
         let doc = document.getElementsByTagName("body")[0]
-        doc.innerHTML += "<h2>ВЫ СТАЛИ ДВОРНИКОМ</h2>"
-    },5000)
+        let Info = document.createElement("h1")
+        doc.appendChild(Info)
+        TimeoutTypeoutHandler(Info,"Вы стали дворником!",100)
+    },1000)
+    shot = false
+    document.getElementsByClassName("SpecialEffectBlock")[0].className = "SpecialEffectActivation"
 }
+
+function TimeoutTypeoutHandler(obj,text,delay){
+    obj.innerHTML += text[0]
+    if (text.substr(1) != ""){
+        setTimeout(TimeoutTypeoutHandler,delay,obj,text.substr(1),delay)
+    }
+}
+
+
+// var string = "a";
+// s = string.substr(1);
+// console.log(s=="")
